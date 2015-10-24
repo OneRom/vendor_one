8d0a9cd1ca1f1a5053361987e15b65d440f9fcc8
Karan Srivastava <theofficiallysonyrebel@gmail.com>
2015-10-24 23:03:53 +0530

    Fix up path for Bootanim

--- configs/bootanim.mk
+++ configs/bootanim.mk
@@ -1,6 +1,6 @@
 # TODO: resolution specific Bootanims
 # Add Bootanim
 PRODUCT_COPY_FILES += \
-    vendor/orm/prebuilt/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+    vendor/orm/prebuilt/bootanim/bootanimation.zip:system/media/bootanimation.zip
 
 
